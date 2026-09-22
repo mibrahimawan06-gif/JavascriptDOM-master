ea743078695eb87bcfaff69b29a03e660ce88564
mibrahimawan06-gif <m.ibrahim.awan06@gmail.com>
2026-09-22 10:26:05 +0200

    Task 1 - createLi function

--- JavascriptDOM-master/js/index.js
+++ JavascriptDOM-master/js/index.js
@@ -1,2 +1,20 @@
 //Write your Javascript code here
 console.log("Shoppinglist")
+
+//task 1
+
+function createLi(className, id, text) 
+{
+    let newLi = document.createElement("li");
+
+    newLi.setAttribute("class", className);
+    newLi.setAttribute("id", id);
+
+    let textNode = document.createTextNode(text);
+    newLi.appendChild(textNode);
+
+    return newLi;
+}
+
+let testLi = createLi("healthy", "five", "Banana");
+console.log(testLi);
